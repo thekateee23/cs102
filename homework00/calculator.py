1a216a47817a0ee873a204f97536864203d9277f
thekateee23 <makhotkina.ekaterina04@yandex.ru>
2022-11-03 22:09:18 +0300

    Добавлено решение

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -103,47 +103,109 @@ def match_case_calc2(num_1: float, command: str) -> tp.Union[float, str]:
                 return math.log10(num_1)
             else:
                 return "десятичный логарифм должен быть строго больше нуля"
+        case "ln":
+            if num_1 > 0:
+                return math.log(num_1)
+            else:
+                return "натуральный логарифм должен быть строго больше нуля"
         case _:
             return f"неизвестный оператор: {command!r}."
     return f"неизвестный оператор: {command!r}."
 
 
-def calc(s: str) -> int:
-    num = 0
-    operat = "+"
-    K = []
+def count_result(opers: list[str]) -> str:
+    operators_1 = ("+", "-", "/", "*")
+    operators_2 = ("sin", "cos", "tg", "lg", "ln")
+    if opers[0] == "-" and (opers[1].isdigit() or "." in opers[1]):
+        opers[0:2] = ["-" + opers[1]]
 
-    def helper(operat, num):
-        if operat == "+":
-            K.append(num)
-        elif operat == "-":
-            K.append(-num)
-        elif operat == "*":
-            K.append(K.pop() * num)
-        elif operat == "/":
-            K.append(int(K.pop() / num))
+    for i in range(2, len(opers)):
+        if (
+            not (opers[i].isdigit() or "." in opers[i])
+            and opers[i - 1] == "-"
+            and (opers[i].isdigit() or "." in opers[i])
+        ):
+            opers[i - 1 : i + 1] = ["-" + opers[i]]
 
-    for i in range(len(s)):
-        if s[i].isdigit():
-            num = num * 10 + int(s[i])
-        elif s[i] == "(":
-            K.append(operat)
-            num = 0
-            operat = "+"
-        elif s[i] in ["+", "-", "*", "/", ")"]:
-            helper(operat, num)
-            if s[i] == ")":
-                num = 0
-                while isinstance(K[-1], int):
-                    num += K.pop()
-                operat = K.pop()
-                helper(operat, num)
-            num = 0
-            operat = s[i]
+    for i in operators_2:
+        while i in opers:
+            ind = opers.index(i)
+            opers[ind : ind + 2] = [match_case_calc2(float(opers[ind + 1]), opers[ind])]
+    while "**" in opers:
+        ind = opers.index("**")
+        opers[ind - 1 : ind + 2] = [float(opers[ind - 1]) ** float(opers[ind + 1])]
+    while "/" in opers or "*" in opers:
+        ind_1 = 10**9
+        ind_2 = 10**9
+        if "/" in opers:
+            ind_1 = opers.index("/")
+        if "*" in opers:
+            ind_2 = opers.index("*")
+        if ind_1 < ind_2:
+            opers[ind_1 - 1 : ind_1 + 2] = [float(opers[ind_1 - 1]) / float(opers[ind_1 + 1])]
+        else:
+            opers[ind_2 - 1 : ind_2 + 2] = [float(opers[ind_2 - 1]) * float(opers[ind_2 + 1])]
+    while "+" in opers or "-" in opers and len(opers) > 2:
+        ind_1 = 10**9
+        ind_2 = 10**9
+        if "+" in opers:
+            ind_1 = opers.index("+")
+        if "-" in opers:
+            ind_2 = opers.index("-")
+        if ind_1 < ind_2:
+            opers[ind_1 - 1 : ind_1 + 2] = [float(opers[ind_1 - 1]) + float(opers[ind_1 + 1])]
+        else:
+            opers[ind_2 - 1 : ind_2 + 2] = [float(opers[ind_2 - 1]) - float(opers[ind_2 + 1])]
+    if opers[0] == "-":
+        opers = [str(-1 * float(opers[1]))]
+    return str(opers[0])
 
-    helper(operat, num)
 
-    return sum(K)
+def calc(comm: str) -> tp.Optional[tp.Tuple[str, int]]:
+    comm = comm.replace("^", "**")
+    operators_1 = ("+", "-", "/", "*", "convert", "**")
+    operators_2 = ("sin", "cos", "tg", "log", "lg", "ln")
+    comm = "".join(comm.split())
+    count = 0
+    operations = []
+    while count < len(comm):
+        if not comm[count].isdigit():
+            if comm[count] == "(":
+                tmp = calc(comm[count + 1 :])
+                if len(tmp) == 2:
+                    tmp, count_tmp = calc(comm[count + 1 :])
+                    count += count_tmp + 1
+                    operations.append(tmp)
+                else:
+                    return None
+            elif comm[count] == ")":
+                count += 1
+                return count_result(operations), count
+            else:
+                operation = comm[count]
+                count += 1
+                while (
+                    count < len(comm)
+                    and len(operation) < 3
+                    and not (operation in operators_1 or operation in operators_2)
+                ):
+                    operation += comm[count]
+                    count += 1
+                if operation == "*" and comm[count] == "*":
+                    count += 1
+                    operation = "**"
+                if count >= len(comm) or len(operation) > 3:
+                    return None
+                operations.append(operation)
+        else:
+            dig = comm[count]
+            dop = ""
+            count += 1
+            while count < len(comm) and (comm[count].isdigit() or comm[count] == "."):
+                dig += comm[count]
+                count += 1
+            operations.append(dig)
+    return count_result(operations), count
 
 
 if __name__ == "__main__":
@@ -161,17 +223,13 @@ if __name__ == "__main__":
         elif COMMAND in operators_2:
             NUM_1 = float(input("Введите число > "))
             print(match_case_calc2(NUM_1, COMMAND))
-        elif (
-            COMMAND.count("+") + COMMAND.count("-") + COMMAND.count("/") + COMMAND.count("*") + COMMAND.count(")")
-        ) >= 1:
-            f = 0
-            for elem in COMMAND:
-                if elem.isdigit() or (elem in "+-()/*"):
-                    continue
-                else:
-                    f = 1
-                    break
-            if f == 0:
-                print(calc(COMMAND))  # было calcul
+        elif len(COMMAND) >= 3:
+            result = calc(COMMAND)
+            if len(result) == 2:
+                print(calc(COMMAND)[0])
             else:
                 print("Неправильный формат ввода")
+        else:
+            NUM_1 = 0
+            NUM_2 = 0
+            print(match_case_calc1(NUM_1, NUM_2, COMMAND))
