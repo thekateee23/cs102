4194e9c47f25ff96b839b56c2acba0deb62fa163
thekateee23 <makhotkina.ekaterina04@yandex.ru>
2022-12-01 12:39:20 +0300

    rsashka

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -53,10 +53,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
