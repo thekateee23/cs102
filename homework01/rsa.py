e7d0dfbd531bbce349fc8a6df2244022fd2a51ba
thekateee23 <makhotkina.ekaterina04@yandex.ru>
2022-12-01 12:56:32 +0300

    rsa миллионная попытка

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -45,6 +45,7 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     y = pow(e, -1, phi)
     return y
 
+
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
