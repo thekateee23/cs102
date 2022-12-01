0d52831587ff75d32b8afd21938c10b4c69cb0a1
thekateee23 <makhotkina.ekaterina04@yandex.ru>
2022-12-01 16:29:40 +0300

    Добавлено решение

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,30 +9,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    while len(keyword) < len(plaintext):
-        keyword *= 2
+    a_low = ord("a")
+    a_up = ord("A")
+    alpha_length = ord("z") - ord("a") + 1
+    shift = [keyword[i % len(keyword)] for i in range(len(plaintext))]
 
-    ls = [ord(i) for i in list(plaintext)]
-    keys = [ord(j) for j in list(keyword)]
-
-    for i in range(len(ls)):
-
-        if 65 <= keys[i] <= 90:
-            keys[i] -= 65
-        elif 97 <= keys[i] <= 122:
-            keys[i] -= 97
-
-        if 97 <= ls[i] <= 122:
-            ls[i] += keys[i]
-            if ls[i] > 122:
-                ls[i] -= 26
-        elif 65 <= ls[i] <= 90:
-            ls[i] += keys[i]
-            if ls[i] > 90:
-                ls[i] -= 26
-
-    ciphertext = "".join(chr(i) for i in ls)
-
+    for i, symbol in enumerate(plaintext):
+        if symbol.isalpha():
+            if symbol.isupper():
+                ciphertext += chr((ord(symbol) + ord(shift[i])) % alpha_length + a_up)
+            elif symbol.islower():
+                ciphertext += chr((ord(symbol) + ord(shift[i]) - 2 * a_low) % alpha_length + a_low)
+        else:
+            ciphertext += symbol
     return ciphertext
 
 
@@ -47,26 +36,17 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    while len(keyword) < len(ciphertext):
-        keyword *= 2
+    a_low = ord("a")
+    a_up = ord("A")
+    alpha_length = ord("z") - ord("a") + 1
+    shift = [keyword[i % len(keyword)] for i in range(len(ciphertext))]
 
-    ls = [ord(i) for i in list(ciphertext)]
-    keys = [ord(i) for i in list(keyword)]
-
-    for i in range(len(ls)):
-        if 65 <= keys[i] <= 90:
-            keys[i] -= 65
-        elif 97 <= keys[i] <= 122:
-            keys[i] -= 97
-
-        if 65 <= ls[i] <= 90:
-            ls[i] -= keys[i]
-            if ls[i] < 65:
-                ls[i] += 26
-        elif 97 <= ls[i] <= 122:
-            ls[i] -= keys[i]
-            if ls[i] < 97:
-                ls[i] += 26
-
-    plaintext = "".join(chr(i) for i in ls)
+    for i, symbol in enumerate(ciphertext):
+        if symbol.isalpha():
+            if symbol.isupper():
+                plaintext += chr((ord(symbol) - ord(shift[i]) - 2 * a_up) % alpha_length + a_up)
+            elif symbol.islower():
+                plaintext += chr((ord(symbol) - ord(shift[i])) % alpha_length + a_low)
+        else:
+            plaintext += symbol
     return plaintext
